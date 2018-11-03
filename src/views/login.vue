bbc9ddedbd285ada6fcf54e1b80d801e4a2f08bc
unknown <24173640@qq.com>
2018-11-03 10:58:40 +0800

    基础框架

---
+++ src/views/login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <p @click="go" class="p1">登录</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+
+    }
+  },
+  methods:{
+    go(){
+      this.$router.push('/index');
+    }
+  }
+}
+</script>
+
+<style scoped>
+.p1{
+  font-size: 0.5rem;
+}
+</style>
